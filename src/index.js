aba9cf296b58db946a97e8c104b0ce59dd9f8809
Roberto Achar <robertoachar@gmail.com>
2017-09-11 17:36:43 -0300

    Adding express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-'use strict';
+const express = require('express');
 
-module.exports.hello = (done) => {
-  return done(null, 'It works!');
-};
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+app.listen(3000, () => {
+  console.log('Express is running on port 3000');
+});
