a778b281a1716f938cc070876ebb1f87a7686796
Roberto Achar <robertoachar@gmail.com>
2017-09-11 21:41:06 -0300

    Adding process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({ message: 'It works!' });
 });
 
-app.listen(3000, () => {
-  console.log('Express is running on port 3000');
+app.listen(port, () => {
+  console.log(`Express is running on port ${port}`);
 });
